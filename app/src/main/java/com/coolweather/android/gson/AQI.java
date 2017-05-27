390ee969517d410719824c81209c330121670bc4
Yc <21592464@qq.com>
2017-05-28 02:28:24 +0800

    加入显示天气信息功能。

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by peekaboo on 2017/5/27.
+ */
+
+public class AQI {
+    public AQIcity city;
+
+    public class AQIcity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+}
